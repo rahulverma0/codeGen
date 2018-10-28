cc7c1649b5c0cf8094daac192cced2f24b475278
Rahul Verma <rahulverma05@yahoo.com>
2018-10-27 17:47:51 -0700

    #FirstCommit - SpringBoot Project Setup

---
+++ src/main/java/com/codegen/devTool/DevToolApplication.java
@@ -0,0 +1,12 @@
+package com.codegen.devTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevToolApplication.class, args);
+	}
+}
