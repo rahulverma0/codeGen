b935767027f9d78431832e96f65b55ceebf3675b
Rahul Verma <rahulverma05@yahoo.com>
2018-10-29 22:10:52 -0700

    #Added the enum for ProtoToTypescript

--- src/main/java/com/codegen/service/protoToTypescript/ProtoToTypescriptEnum.java
+++ src/main/java/com/codegen/service/protoToTypescript/ProtoToTypescriptEnum.java
@@ -9,4 +9,34 @@ package com.codegen.service.protoToTypescript;
  */
 public enum ProtoToTypescriptEnum {
 
+	DOUBLE("double","number"),
+	FLOAT("float","number"),
+	INT32("int32","number"),
+	INT64("int64","number"),
+	UINT32("uint32","number"),
+	UINT64("uint64","number"),
+	SINT32("sint32","number"),
+	SINT64("sint64","number"),
+	FIXED32("fixed32","number"),
+	FIXED64("fixed64","number"),
+	SFIXED32("sfixed32","number"),
+	SFIXED64("sfixed64","number"),
+	BOOL("bool","boolean"),
+	STRING("string","string"),
+	BYTE("bytes","string");
+	
+	private final String key;
+    private final String value;
+    
+    ProtoToTypescriptEnum(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+    public String getValue() {
+        return value;
+    }
 }
